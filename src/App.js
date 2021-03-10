07cbbc91dac03378b1a5c0cde19a84ea455c2077
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-10 11:53:00 +0200

    api is added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, { useEffect } from 'react'
 import './App.css';
 import CurrencyRow from './CurrencyRow'
 
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <>
       <h1>Converter</h1>
